c08d18dc99af3fb0d50101ea8d44df142d77064a
Xavier Grand <grand.xavier@gmail.com>
2014-05-23 12:00:01 +0200

    Add assert for the parametter length

--- src/main/java/com/algolia/firebase/connector/Connector.java
+++ src/main/java/com/algolia/firebase/connector/Connector.java
@@ -14,6 +14,7 @@ public class Connector {
 		String[] params = null;
 		if (App.getDocManagerParam() != null) {
 			params = App.getDocManagerParam().split(",");
+			assert(params.length == url.length);
 		}
 		tables = new Firebase[url.length];
 		listeners = new DocManager[url.length];
